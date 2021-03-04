6b5ba971ccee9dcecbdc09cb81a607612e387d50
mansoor250984 <mansoor250984@gmail.com>
2021-03-04 22:47:00 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,6 +8,29 @@ $(".navigation-menu .fa-times").click(function(){
     $(".nav div.navigation-menu, .overlay").toggle(500);
 	$('body').css('overflow-y', 'visible');
 });
+
+$(".tab1").click(function(){
+    $(".tabber1").fadeIn(500);
+	$(".tabber2, .tabber3, .tabber4").fadeOut(0);
+});
+$(".tab2").click(function(){
+    $(".tabber2").fadeIn(500);
+	$(".tabber1, .tabber3, .tabber4").fadeOut(0);
+});
+$(".tab3").click(function(){
+    $(".tabber3, .tabber5").fadeIn(500);
+	$(".tabber1, .tabber2, .tabber4").fadeOut(0);
+});
+$(".tab4").click(function(){
+	$(".tabber1, .tabber2, .tabber3").fadeOut(0);
+	$(".tabber4, .tabber5").fadeIn(500);
+});
+
+$('.navigation li a').on('click', function(){
+    $('.navigation li a.active').removeClass('active');
+    $(this).addClass('active');
+});
+
+
     });
-	
 	
